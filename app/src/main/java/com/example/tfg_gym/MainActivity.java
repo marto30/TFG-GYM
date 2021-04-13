00014407245e3f8078894b53e66dd3bb39fcc2f6
marto <martoperello@gmail.com>
2021-04-13 22:21:39 +0200

    vistas 2

--- app/src/main/java/com/example/tfg_gym/MainActivity.java
+++ app/src/main/java/com/example/tfg_gym/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.tfg_gym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.perfil_cliente);
+        SharedPreferences mPrefs =  PreferenceManager.getDefaultSharedPreferences(this);
+        this.isNightModeEnabled = mPrefs.getBoolean(“NIGHT_MODE”, false);
     }
+
+    public boolean isNightModeEnabled() {
+        return isNightModeEnabled;
+    }
+
+    public void setIsNightModeEnabled(boolean isNightModeEnabled) {
+        this.isNightModeEnabled = isNightModeEnabled;
+    }
+
 }
