a276f5488d129a753f69b93da729da0ce7430724
marto <martoperello@gmail.com>
2021-04-17 13:08:22 +0200

    cambios unido - solcionar errores

---
+++ app/src/main/java/com/example/tfg_gym/Activities/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tfg_gym.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tema7.tema7ejemplo2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.AppTheme);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
