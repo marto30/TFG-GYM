4d13e15fa0e1949d35e39a4a8bed0a565f17726c
marto <martoperello@gmail.com>
2021-05-19 23:49:51 +0200

    Semifinal

---
+++ app/src/main/java/com/tema7/tema7ejemplo2/Adapter/InstructoresAdapter.java
@@ -0,0 +1,14 @@
+package com.tema7.tema7ejemplo2.Adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstructoresAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructores_adapter);
+    }
+}
--- app/src/main/java/com/tema7/tema7ejemplo2/Fragments/instrucoresFragment.java
+++ app/src/main/java/com/tema7/tema7ejemplo2/Fragments/instrucoresFragment.java
@@ -1,4 +1,4 @@
-package com.tema7.tema7ejemplo2.Fragments;
+package com.tema7.tema7ejemplo2;
 
 import android.os.Bundle;
 
@@ -8,14 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.tema7.tema7ejemplo2.R;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FavoritoFragment#newInstance} factory method to
+ * Use the {@link instrucoresFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FavoritoFragment extends Fragment {
+public class instrucoresFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +24,7 @@ public class FavoritoFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FavoritoFragment() {
+    public instrucoresFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +34,11 @@ public class FavoritoFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FavoritoFragment.
+     * @return A new instance of fragment instrucoresFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static FavoritoFragment newInstance(String param1, String param2) {
-        FavoritoFragment fragment = new FavoritoFragment();
+    public static instrucoresFragment newInstance(String param1, String param2) {
+        instrucoresFragment fragment = new instrucoresFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +59,6 @@ public class FavoritoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorito, container, false);
+        return inflater.inflate(R.layout.fragment_instrucores, container, false);
     }
 }
